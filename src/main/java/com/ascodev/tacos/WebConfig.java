433850ede4d26e5dfc41d58aa9c2f5852f145637
ascodev <zhuravlovstepan32@gmail.com>
2025-01-09 23:17:26 +0100

    Build: Orders page / Design taco page: added controllers: OrderController; DesignTacoController; IngredientByIdConverter// Update: Taco / TacoOrder: added validation for these entities//

---
+++ src/main/java/com/ascodev/tacos/WebConfig.java
@@ -0,0 +1,16 @@
+package com.ascodev.tacos;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("home");
+    }
+
+}
